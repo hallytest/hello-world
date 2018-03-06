a020f09329f5eabda0bf52f0c72d80b15d5fc803
G G <gabbe1234567899@gmail.com>
2018-03-06 23:20:44 +0100

    First commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: 6 mars 2018
+ *      Author: Gabi
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
